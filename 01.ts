3d2901425e3e5a5e64de466d2d5f5831faded6d5
slamflipstrom <lindstrom.s@gmail.com>
2022-12-20 17:58:45 -0800

    01 part 2

--- 01.ts
+++ 01.ts
@@ -3,20 +3,35 @@ const input = readFileSync("./01-puzzle-input.txt", "utf8")
 
 const inputArray = input.split("\n")
 
-// iterate through inputArray until we find an empty string
-let largestVal = 0
+let first = 0
+let second = 0
+let third = 0
+
 let sum = 0
 
-inputArray.forEach((item: string, index) => {
+inputArray.forEach((item: string) => {
+  // if item is not an empty string, add to sum
   if (item !== "") {
     sum += parseInt(item)
   }
 
+  // if item is an empty string, check if sum is larger than first
   if (item === "") {
-    if (sum > largestVal) {
-      largestVal = sum
+    if (sum > first) {
+      third = second
+      second = first
+      first = sum
+    } else if (sum > second) {
+      third = second
+      second = sum
+    } else if (sum > third) {
+      third = sum
     }
+
+    // reset sum
     sum = 0
   }
 })
-console.log("largestVal:", largestVal)
+
+console.log("Largest: ", first)
+console.log("Top 3 Sum: ", first + second + third)
