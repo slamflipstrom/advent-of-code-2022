1e3f0e20161f02e7f8bdef613a99a91441e38831
slamflipstrom <lindstrom.s@gmail.com>
2022-12-22 19:41:15 -0800

    psuedocode

--- 03/03.ts
+++ 03/03.ts
@@ -1,2 +1,12 @@
 import { readFileSync } from "fs"
 const input = readFileSync("./03/03-input.txt", "utf8")
+
+const inputArray = input.split("\n")
+
+// split each line into equal strings, regardless of length
+
+// find matching chars
+
+// map chars to priority scale
+
+// sum priorities of each line's matching char
