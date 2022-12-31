a0c60c3132e7ec1a5360f43a23f16cbf517775de
slamflipstrom <lindstrom.s@gmail.com>
2022-12-30 18:37:32 -0800

    solve for day 4 part 1 - shitty

--- 04/04.ts
+++ 04/04.ts
@@ -3,18 +3,37 @@ const input = readFileSync("./04/04-input.txt", "utf8")
 
 const inputArray = input.split("\n")
 
-const checkRange = (mins: string[], maxs: string[]) => {
-  let count = 0
+const checkRange = (value: string, min: string, max: string): boolean => {
+  if (Number(value) >= Number(min) && Number(value) <= Number(max)) {
+    return true
+  }
+  return false
 }
 
 const processInput = () => {
+  let count = 0
   inputArray.forEach((item) => {
     //split item by comma
     const [first, second] = item.split(",")
     const [firstMin, firstMax] = first.split("-")
     const [secondMin, secondMax] = second.split("-")
-    checkRange([firstMin, secondMin], [firstMax, secondMax])
+
+    //check if first is in second range
+    if (checkRange(firstMin, secondMin, secondMax)) {
+      if (checkRange(firstMax, secondMin, secondMax)) {
+        count++
+        return
+      }
+    }
+
+    //check if second is in first range
+    if (checkRange(secondMin, firstMin, firstMax)) {
+      if (checkRange(secondMax, firstMin, firstMax)) {
+        count++
+      }
+    }
   })
+  console.log("count: ", count)
 }
 
 processInput()
